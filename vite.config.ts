ab2608ac619c922f20306595d39dd76584cb9678
Jon Bảo <dinhgiabao7665@gmail.com>
2026-04-26 21:38:45 +0700

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,13 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/Bugbo/',
     plugins: [
       react(),
     ],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
     },
     resolve: {
       alias: {
